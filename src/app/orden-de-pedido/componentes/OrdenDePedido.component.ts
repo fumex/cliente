e53a28e07e2f200282c7f64170caffad6244afc9
miguel angel <migueljeaguer@gmail.com>
2018-05-30 11:14:35 -0500

    modificacion pedidos

--- src/app/orden-de-pedido/componentes/OrdenDePedido.component.ts
+++ src/app/orden-de-pedido/componentes/OrdenDePedido.component.ts
@@ -35,7 +35,8 @@ export class OrdenDePedidoComponent{
     public mostrarguardar;
     public id;
     public editar;
-    public sielnombreesigual;
+    public nombres;
+    public quitar;
     constructor(
         private _almacenesService:AlmacenesService,
         private _route:ActivatedRoute,
@@ -46,6 +47,7 @@ export class OrdenDePedidoComponent{
         private _proveedorservice:ProveedorService
     ){
         this.titulo="Orden De Pedido";
+        
         this.agregarOrdenPedido=new OrdenDePedidoModel(0,null,null,0,null);
         this.agregarDetalleOrden=new DetalleOrdenDePedidoModel(0,0,'',0);
         this.fecha=null;
@@ -54,7 +56,7 @@ export class OrdenDePedidoComponent{
         this.mostrarguardar=null;
         this.id=0;
         this.editar=null;
-        this.sielnombreesigual=0;
+        this.quitar=null;
     }
     ngOnInit(){
         this.mostraralmacen();
@@ -109,32 +111,75 @@ export class OrdenDePedidoComponent{
     }
     cambio(){
         this.mostrar=1;
+        this.tabla();
         console.log(this.mostrar);
     }
-    addpedido(nombre,c){
+    ocultar(){
+        this.mostrar=null;
+    }
+    deletepedido(idindice){
+        this.mostrarguardar=this.mostrarguardar-1;
+        
+        console.log(this.productos[idindice].id);
+        //this.pedidos.splice(index,1);
+        while(this.id<this.pedidos.length){
+            if(this.pedidos[this.id].id===idindice)
+            {
+                this.productos[idindice].id=this.pedidos[this.id].id_orden_pedido;
+                this.pedidos.splice(this.id,1);
+            }
+            this.id=this.id+1;
+        }
+        this.id=0;
+    }
+    addpedido(nombre,idente,idpro){
+        console.log(idpro);
         this.mostrarguardar=this.mostrarguardar+1;
-        /*if(this.pedidos.length>0)
-        {
+        this.productos[idente].id=this.quitar;
+        this.agregarDetalleOrden.id=idente;
+        this.agregarDetalleOrden.id_producto=nombre;
+        this.agregarDetalleOrden.id_orden_pedido=idpro;
+        this.pedidos.push(this.agregarDetalleOrden);
+        this.agregarDetalleOrden=new DetalleOrdenDePedidoModel(null,null,null,null);
+        console.log(this.pedidos);
+        console.log(this.productos);
+       /* this.id=0;
+        this.sielnombreesigual=0;
+        
+        this.nombres=nombre;
+        if(this.pedidos.length>=1){
             while(this.id<this.pedidos.length){
             this.id=this.id+1;
-            if(this.pedidos[this.id-1].id_producto==nombre)
-            {
-                this.sielnombreesigual=this.id;
-                this.id=this.pedidos.length;
-            }
-
+            console.log(this.pedidos[this.id-1].id_producto,this.id);
+                if(this.nombres===this.pedidos[this.id-1].id_producto.toString())
+                {
+                   this.sielnombreesigual=this.id-1;
+                }else{
+                    
+                }
             }
         }
         
-
         if(this.sielnombreesigual>0){
-            this.agregarDetalleOrden.cantidad=this.pedidos[this.sielnombreesigual].cantidad+c;
-        }**/
-            
-        this.agregarDetalleOrden.id_producto=nombre;
-        this.agregarDetalleOrden.cantidad=c;
-        this.pedidos.push(this.agregarDetalleOrden);
-        this.agregarDetalleOrden=new DetalleOrdenDePedidoModel(null,null,null,null);
+            //this.agregarDetalleOrden.cantidad=this.pedidos[this.sielnombreesigual].cantidad+c;
+            console.log(this.sielnombreesigual);
+            this.agregarDetalleOrden.id_producto=nombre;
+            this.agregarDetalleOrden.cantidad=parseInt(this.pedidos[this.sielnombreesigual].cantidad.toString()) +parseInt(c);
+            this.pedidos.splice(this.sielnombreesigual-1,1);
+            this.pedidos.push(this.agregarDetalleOrden);
+            console.log(this.pedidos);
+            this.sielnombreesigual=0;*/
+        
+    }
+    limpiar(){
+        while(this.id<this.pedidos.length){
+            this.productos[this.pedidos[this.id].id].id=this.pedidos[this.id].id_orden_pedido;
+            this.pedidos.splice(this.id,1);
+        }
+        this.mostrarguardar=0;
+        this.id=0;
+        this.mostrar=null;
+        this.agregarOrdenPedido=new OrdenDePedidoModel(0,null,null,0,null);
     }
     guardartodo(){
         this._ordenPedidoService.addOrdenPedido(this.agregarOrdenPedido).subscribe(
@@ -163,8 +208,13 @@ export class OrdenDePedidoComponent{
         }
         this.id=0;
     }
-    exitCompra(index){
+    exitCompra(index,idpedi){
+        console.log(this.pedidos[index].id_orden_pedido);
+        console.log(index,idpedi);;
+        this.mostrarguardar=this.mostrarguardar-1;
+        this.productos[idpedi].id=this.pedidos[index].id_orden_pedido;
         this.pedidos.splice(index,1);
+        console.log(idpedi);
     }
     editarcantidad(indi){
         console.log(indi);
@@ -177,5 +227,17 @@ export class OrdenDePedidoComponent{
     }
     cancelar(){
         this.editar=null;
+    }
+    tabla(){
+        setTimeout(function(){
+            $(function(){
+                 $('#mytable').DataTable({
+                     dom: 'Bfrtip',
+                     buttons: [
+                        
+                     ]
+                 });
+            });
+        },500);
     }
 }
